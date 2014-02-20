271652b9047e0ec6ad2d4dfdcec6dd7f60f7042a
timor <timor.dd@googlemail.com>
2014-02-20 23:19:59 +0100

    token threading working

--- interpreter.c
+++ interpreter.c
@@ -32,23 +32,27 @@
 #define VM_CSTACK 64
 #endif
 
-#define MEM_BASE 0x20000000
-
 /* extern int putchar(int); */
 /* extern int getchar(int); */
+#ifndef INSTBASE
+#warning "defining INSTBASE for x64!"
+#define INSTBASE 0x80
+#endif
 
-enum inst {
-  dup,
-  rot,
-  drop,
-  blah=900
+/* primitive instruction set */
+enum inst_code {
+  dup=INSTBASE,                     /* starting value architecture dependent!!! */
+  rot, drop, zero, one, two, add, mul, neg, sub, emit, claim, to_r, r_from, lit,
+  quit
 };
+
+typedef unsigned char inst;     /* it's byte code after all */
 
 typedef enum cell_type
 {
   cell_fixnum = 0,
   cell_sequence,
-  cell_string,					  /* value is  */
+  cell_string,					  /* value is number of chars following */
   cell_bignum,
   cell_code,
   cell_symbol
@@ -60,18 +64,20 @@ typedef struct cell
   cell_type type:3;
 } cell;
 
+/* somehow the following doesnt count as constant value for static initializer purposes */
 #define FIXNUM(c) ((cell) {.type=cell_fixnum,.value=c})
   
 
 typedef struct dict_entry
 {
-  cell * address;
-  cell name;						  /* a string */
+  void * address;               /* pointer into memory */
+  cell name;						  /* a cell_string */
 } dict_entry;
 
+/* empty ascending stack */
 #define push(sp,val) (*(sp++)=val)
 #define pop(sp) (*(--sp))
-#define peek(sp) (*sp)
+#define peek(sp) (*(sp-1))
 
 static void push_mem(cell *from, cell *to, cell *sp)
 {
@@ -84,47 +90,58 @@ static void push_mem(cell *from, cell *to, cell *sp)
 
 void interpreter()
 {
-  static unsigned char iprog[] = {dup,dup,rot,(900)&0xff, (900>>8)&0xff};
   cell pstack[VM_PSTACK]={0};
   cell* psp = &pstack[0];
   cell rstack[VM_RSTACK]={0};
   cell* rsp = &rstack[0];
   cell cstack[VM_CSTACK]={0};
   cell* csp = &cstack[0];
-#define next goto **(++pc)
+
+#define next goto **(--pc)
+#define BINOP(op) { x = pop(psp); x.value=(pop(psp).value op x.value); push(psp,x);} break
+  
   cell x;
-  void *program[]={&&lit, (void*)'A', &&putchar, &&quit};
-  void **pc = program;
-  goto **pc;
+  inst program[]={quit,emit,mul,add,two,two,mul,dup,mul,dup,two};
+  inst *pc = &program[sizeof(program)/sizeof(inst)-1];
   unsigned int sequence_counter=0;
 
-drop:
-  x = pop(psp);
-  if (x.type != cell_fixnum)
-    for (int i = 0; i < x.value; i++)
-      (void) pop(psp);
-  next;
+  while(1) switch (*pc--){
+    case drop:
+      x = pop(psp);
+      if (x.type != cell_fixnum)
+        for (int i = 0; i < x.value; i++)
+          (void) pop(psp);
+      break;
+    case zero: push(psp,FIXNUM(0)); break;
+    case one: push(psp,FIXNUM(1)); break;
+    case two: push(psp,FIXNUM(2)); break;
+    case add: BINOP(+);
+    case mul: BINOP(*);
+    case sub: BINOP(-);
+    case dup:
+      x = peek(psp);
+      if (x.type == cell_fixnum)
+        push(psp, x);
+      else
+        push_mem(psp-(x.value+1),psp,psp);
+      break;
 
-dup:
-  x = peek(psp);
-  if (x.type == cell_fixnum)
-    push(psp, x);
-  else
-    push_mem(psp-(x.value+1),psp,psp);
-  next;
+    case lit:
+      push(psp,*((cell*)++pc));
+      break;
 
-lit:
-  push(psp,*((cell*)++pc));
-  next;
+    case emit:
+      putchar(pop(psp).value);
+      fflush(stdout);
+      break;
 
-putchar:
-  putchar(pop(psp).value);
-  fflush(stdout);
-  next;
-
-quit:
-  return;
+    case quit:
+      printf("exiting\n");
+      return; break;
+    default:
+      printf("unknown instruction %d\n",*pc);
+      return;
+    }
 }
-
-
+  
 #endif
