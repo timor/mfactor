74b78a3c6d0bc8ce738dff843053157ba91258c4
timor <timor.dd@googlemail.com>
2014-02-21 23:36:51 +0100

    remove type info from cell, fix multi-target

--- interpreter.c
+++ interpreter.c
@@ -19,6 +19,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifndef VM_DICT
 #define VM_DICT 8192
@@ -36,8 +37,13 @@
 /* extern int putchar(int); */
 /* extern int getchar(int); */
 #ifndef INSTBASE
-#warning "defining INSTBASE for x64!"
+#if (__linux && __LP64__)
 #define INSTBASE 0x80
+#elif (CPU_LPC4337)
+#define INSTBASE 0x80
+#else
+#error "don't know instruction code base for architecure!"
+#endif
 #endif
 
 /* primitive instruction set */
@@ -50,27 +56,20 @@ enum inst_code {
 
 typedef unsigned char inst;     /* it's byte code after all */
 
-typedef enum cell_type
+typedef union cell
 {
-  cell_fixnum = 0,
-  cell_sequence,
-  cell_string,					  /* value is number of chars following */
-  cell_bignum,
-  cell_code,
-  cell_symbol,
-  cell_typecell                 /* meta!(for introspection) */
-} cell_type;
-
-typedef struct cell
-{
-  unsigned int value:29;
-  cell_type type:3;
+  struct {
+    unsigned int length:31;
+    bool sequencep:1;
+  };
+  struct {
+    int value:31;
+    bool ____dummy1oneone:1;
+  };
 } cell;
 
 /* somehow the following doesnt count as constant value for static initializer purposes */
-#define FIXNUM(c) ((cell) {.type=cell_fixnum,.value=c})
-#define TYPECELL(c) ((cell) {.type=cell_typecell,.value=c})
-  
+#define FIXNUM(c) ((cell) {.sequencep=false,.value=c})
 
 typedef struct dict_entry
 {
@@ -92,34 +91,39 @@ static void push_mem(cell *from, cell *to, cell *sp)
     memcpy(sp-diff,to,-diff);
 }
 
-union wide_cell 
+typedef union wide_cell 
 {
-  cell cell;
   inst * address;
+  cell cell;
 } wide_cell;
-
-inst square[]={exit,mul,dup};
-
-void interpreter()
-{
-  cell pstack[VM_PSTACK]={0};
-  cell* psp = &pstack[0];
-  union wide_cell rstack[VM_RSTACK]={0};
-  union wide_cell* rsp = &rstack[0];
-  cell cstack[VM_CSTACK]={0};
-  cell* csp = &cstack[0];
-  
 
 #define UNOP(op) { x = pop(psp); x.value=(op x.value); push(psp,x);} break
 #define BINOP(op) { x = pop(psp); x.value=(pop(psp).value op x.value); push(psp,x);} break
 #define TBEGIN(word) (intptr_t)&word[sizeof(word)/sizeof(inst)-1]
 #define CALL64(word) (word>>0)&0xff,(word>>8)&0xff,(word>>16)&0xff,(word>>24)&0xff,(word>>32)&0xff,(word>>40)&0xff,(word>>48)&0xff,(word>>56)&0xff
+#define CALL32(word) (word>>0)&0xff,(word>>8)&0xff,(word>>16)&0xff,(word>>24)&0xff
+#if (__SIZEOF_POINTER__ == 4)
+#define CALL(thread) CALL32(TBEGIN(thread))
+#elif (__SIZEOF_POINTER__ == 8)
 #define CALL(thread) CALL64(TBEGIN(thread))
-  
-  cell x;
+#else
+#error "size of pointer unkown"
+#endif
+
+inst square[]={exit,mul,dup};
+
+
+void interpreter()
+{
+  cell pstack[VM_PSTACK]={0};
+  cell* psp = &pstack[0];
+  wide_cell rstack[VM_RSTACK]={0};
+  wide_cell* rsp = &rstack[0];
+  cell cstack[VM_CSTACK]={0};
+  cell* csp = &cstack[0];
+  cell x;                       /* temporary value for operations */
   inst program[]={quit,emit,CALL(square),add,dup,mul,dup,two};
   inst *pc = &program[sizeof(program)/sizeof(inst)-1];
-
 
   while(1) {
     inst i = (*pc--);
@@ -127,8 +131,8 @@ void interpreter()
       switch (i) {
       case drop:
         x = pop(psp);
-        if (x.type != cell_fixnum)
-          for (int j = 0; j < x.value; j++)
+        if (x.sequencep)
+          for (int j = 0; j < x.length; j++)
             (void) pop(psp);
         break;
       case zero: push(psp,FIXNUM(0)); break;
@@ -140,10 +144,10 @@ void interpreter()
       case neg: UNOP(-);
       case dup:
         x = peek(psp);
-        if (x.type == cell_fixnum)
+        if (!x.sequencep)
           push(psp, x);
         else
-          push_mem(psp-(x.value+1),psp,psp);
+          push_mem(psp-(x.length+1),psp,psp);
         break;
       case lit:
         push(psp,*((cell*)++pc));
@@ -152,8 +156,8 @@ void interpreter()
         putchar(pop(psp).value);
         fflush(stdout);
         break;
-      case type:
-        push(psp,TYPECELL(pop(psp).type));
+      /* case type: */
+      /*   push(psp,TYPECELL(pop(psp).type)); */
       case name:
       case quit:
         printf("bye!\n");
@@ -161,13 +165,13 @@ void interpreter()
       case exit:
         pc=pop(rsp).address; break;
       default:
-        printf("unknown instruction %d\n",*pc);
+        printf("undefined instruction %d\n",*pc);
         return;
       }
     } else {                    /* memory, call thread  */
       inst * skipped = (inst *)pc-(sizeof(void *)-1); /* adjust skip over memory address */
       inst * next_word = *(inst **)(skipped+1); /* TODO platform-dependent */
-      push(rsp,((union wide_cell) skipped));
+      push(rsp,((wide_cell) skipped));
       pc=next_word;
     }
   }
