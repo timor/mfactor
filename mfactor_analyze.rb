cec7ec9ccbc55a9e055aee2251ab431c408e91e0
timor(@motion) <timor.dd@googlemail.com>
2014-07-30 00:04:23 +0200

    more work on analyzer

--- mfactor_analyze.rb
+++ mfactor_analyze.rb
@@ -1,18 +1,44 @@
 require_relative 'mfactor'
+require 'parslet'
 
 class MFStack < Array
   def swap
-    tmp = self[-1]
-    self[-1]=self[-2]
-    self[-2]=tmp
+    e1 = pop
+    e2 = pop
+    push e1
+    push e2
+  end
+  def pop()
+    if size == 0
+      raise "stack empty!"
+    else
+      super
+    end
   end
   def drop() pop end
-  def dup() push self[-1] end
+  def dup()
+    e = pop
+    push e
+    push e
+  end
 end
 
 class MFStaticCompile
   attr_acccessor :mf
   def initialize(mf)
     @mf=mf
+  end
+  def walk
+    mf.dictionary.values.each do |vocab|
+      vocab.definitions.each do |d|
+        pstack = MFStack.new
+        rstack = MFStack.new
+        if d.normal_word?
+          d.body.each do |word|
+            case word
+            when MFPrim then
+              case word.name
+                when "dup" then 
+                
   end
 end
