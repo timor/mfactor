971effec09561834b720ca62522be11752e880c4
timor <timor.dd@googlemail.com>
2014-09-12 13:20:18 +0200

    add Tokenizer with tests

--- lib/mfactor/eval.rb
+++ lib/mfactor/eval.rb
@@ -3,6 +3,37 @@ require 'mfactor/vocab'
 require 'mfactor/parser'
 
 module MFactor
+  class Token < String
+    attr_reader :line, :column
+    def initialize(str, line, col)
+      super str
+      @line=line
+      @column=col
+    end
+  end
+  class Tokenizer
+    include Enumerable
+    attr_reader :filename
+    def initialize source
+      @filename = (source.is_a? File and source.path)
+      @s=source
+    end
+    def each
+      col=0
+      lnum=0
+      while line=@s.gets
+        line.chomp
+        lnum=lnum+1
+        col=0
+        line=StringIO.new(line)
+        while part=line.gets(" ")
+          token=part.chomp(" ").gsub(/\t/,"")
+          yield Token.new(token, lnum, col) if token != ""
+          col = col + part.length
+        end
+      end
+    end
+  end
   class Eval
     attr_accessor :primitives  # for debugging
     def initialize(a=[])
--- test/test_eval.rb
+++ test/test_eval.rb
@@ -1,6 +1,31 @@
 require 'test/unit'
 require 'mfactor/eval'
 
+class TokenizerTest < Test::Unit::TestCase
+  def string_tokens(s)
+    MFactor::Tokenizer.new(StringIO.new(s)).to_a
+  end
+  def test_tokenize_steps
+    t = MFactor::Tokenizer.new(StringIO.new("3 items here")).to_enum
+    assert_equal "3", t.next
+    assert_equal "items", t.next
+    assert_equal "here", t.next
+  end
+  def test_tokenize_line
+    tokens = string_tokens("this is a  line with 7 	elements.")
+    assert_equal 7,tokens.length
+    tokens = string_tokens("this is a  line with 7 	elements.")
+    assert_equal 7,tokens.length
+  end
+  def test_tokenize_pos
+    a = string_tokens("another test line\nanother  test 	line")
+    assert_equal 6, a.length
+    assert_equal [0,8,13,0,9,14], a.map{|t| t.column}
+    assert_equal 1,a[0].line
+    assert_equal 2,a[-1].line
+  end
+end
+
 class EvalTest < Test::Unit::TestCase
   def test_basic_eval
     e = MFactor::Eval.new
