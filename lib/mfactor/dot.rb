0c9f24ea59dd2332401da799346a8de635af4c10
timor <timor.dd@googlemail.com>
2014-10-05 14:06:01 +0200

    cleaner DotNode mixin interface
    supplies `add_children`, and friends

--- lib/mfactor/dot.rb
+++ lib/mfactor/dot.rb
@@ -9,79 +9,149 @@ module MFactor
     require 'set'
     require 'ostruct'
     @@unique='1'
-
+    def add_child(*clist)
+      puts "child"
+      @child_nodes ||= []
+      clist.each do |c|
+        unless @child_nodes.member? c
+          @child_nodes.push c
+          c.add_parent self
+        end
+      end
+    end
+    def add_parent(*clist)
+      puts "parent"
+      @parent_nodes ||= []
+      clist.each do |c|
+        unless @parent_nodes.member? c
+          @parent_nodes.push c
+          c.add_child self
+        end
+      end
+    end
+    def add_sibling(*clist)
+      puts "sibling"
+      @sibling_nodes ||= []
+      clist.each do |c|
+        unless @sibling_nodes.member? c
+          @sibling_nodes.push c
+          c.add_sibling self
+        end
+      end
+    end
+    def in_record(r)
+      puts "in_record"
+      @record = r
+    end
+    def record
+      @record||=nil
+      @record
+    end
     def gensym(s="G")
       (s.to_s+@@unique.succ!).to_sym
     end
-    # traverse from 1 node, collect all reachable nodes
-    def collect(nodes=Set.new,transitions=Set.new,node_names={},records={})
-      if respond_to? :dot_record
-        rec, pos = dot_record
-        # association: record_object => [ [ label, field_name, position ]* ]
-        field_name="f#{pos}"
-        port_info= OpenStruct.new(label: dot_label,
-                                  name: field_name,
-                                  position: position)
-        if records[rec]
-          records[rec].push port_info
-        else
-          node_names[rec]=gensym("rec")
-          records[rec] = [port_info]
-        end
-        node_names[self]="#{node_names[rec]}:#{field_name}"
-      else
-        nodes.add self
-        node_names[self]=gensym("node")
-      end
-      if respond_to? :child_nodes
-        child_nodes.each do |n|
-          transitions.add [self,n]
-          n.collect(nodes,transitions,node_names,records) unless nodes.member? n
-        end
-      end
-      if respond_to? :parent_nodes
-        parent_nodes.each do |n|
-          transitions.add [n,self]
-          n.collect(nodes,transitions,node_names,records) unless nodes.member? n
-        end
-      end
-      if respond_to? :sibling_nodes
-        sibling_nodes.each do |n|
-          n.collect(nodes,transitions,node_names,records) unless nodes.member? n
-        end
-      end
-      return nodes,transitions,node_names,records
+    module_function :gensym
+    def node_name
+      @name||=gensym("node")
+      @name
     end
-
-    # generate dot code for one record
-    def self.dot_record_code(io, record_name, portinfos)
-      portinfos.sort! {|p1, p2| p1.position <=> p2.position }
-      io << record_name << " [label=\"{"
-      io << portinfos.map do |p|
-        "<#{p.name}> #{p.label}"
-        end.join(" | ")
-      io << "}\"]\n"
+    # traverse from 1 node, collect all reachable nodes
+    def collect(nodes=Set.new,transitions=Set.new,records=Set.new)
+      @child_nodes ||= []
+      @parent_nodes ||= []
+      @sibling_nodes ||= []
+      @record ||= nil
+      nodes.add self
+      if @record
+        puts "set record"
+        records.add @record
+      end
+      @child_nodes.each do |n|
+        puts "collect child: #{n.dot_label}"
+        transitions.add [self,n]
+        n.collect(nodes,transitions,records) unless nodes.member? n
+      end
+      @parent_nodes.each do |n|
+        puts "collect parent: #{n.dot_label}"
+        transitions.add [n,self]
+        n.collect(nodes,transitions,records) unless nodes.member? n
+      end
+      @sibling_nodes.each do |n|
+        puts "collect sibling: #{n.dot_label}"
+        n.collect(nodes,transitions,records) unless nodes.member? n
+      end
+      return nodes,transitions,records
     end
 
     # generate graph from this node on, reachability determined by self
-    def dot_code(io)
+    def dot_graph(io)
       io << <<END
 digraph test_definition {
 graph [ rankdir=LR ]
 node [shape=record,fontname=helvetica]
 END
-      nodes,transitions,node_names,records = collect
+      nodes,transitions,records = collect
       nodes.each do |n|
-        io.puts "#{node_names[n]} [label=\"#{n.dot_label}\"]"
+        next if n.record
+        attrs={:label => '"'+n.dot_label+'"'}
+        if n.respond_to? :dot_node_shape
+          attrs[:shape]='"'+n.dot_node_shape+'"'
+        end
+        attr_string=attrs.map do |k,v|
+          "#{k.to_s}=#{v}"
+        end.join(", ")
+        io.puts "#{n.node_name} [#{attr_string}]"
       end
-      puts records
-      records.each do |rec,ports|
-        DotNode.dot_record_code(io, node_names[rec],ports)
+      records.each do |rec|
+        rec.dot_code(io)
       end
       transitions.each do |s,d|
-        io.puts "#{node_names[s]} -> #{node_names[d]}"
+        sname = s.node_name.to_s
+        dname = d.node_name.to_s
+        if s.record
+          sname = s.record.node_name.to_s+':'+sname
+        end
+        if d.record
+          dname = d.record.node_name.to_s+':'+dname
+        end
+        io.puts "#{sname} -> #{dname}"
       end
       io.puts "}"
+    end
+  end
+
+  module DotRecord
+    def props
+      @port_nodes ||= []
+      @name ||= DotNode.gensym("rec")
+    end
+    def add_port(*clist)
+      props
+      puts "port"
+      clist.each do |c|
+        unless @port_nodes.member? c
+          @port_nodes.push c
+          raise "node already in record" if c.record
+          c.in_record self
+        end
+      end
+    end
+    def node_name
+      props
+      @name
+    end
+    # generate dot code for one record
+    def dot_code(io)
+      props
+      portinfos = @port_nodes.map do |n|
+        OpenStruct.new(name: n.node_name,
+                       label: n.dot_label)
+      end
+      io << node_name << " [label=\"{"
+      io << portinfos.map do |p|
+        "<#{p.name}> #{p.label}"
+        end.join(" | ")
+      io << "}\"]\n"
     end
   end
 end
--- test/test_dot.rb
+++ test/test_dot.rb
@@ -4,69 +4,84 @@ require 'tempfile'
 require 'fileutils'
 include FileUtils
 
-class TestNode < Struct.new(:label, :children, :parents, :siblings)
+class TestNode < Struct.new(:label)
   include MFactor::DotNode
   def dot_label
     label
   end
-  def child_nodes
-    children||[]
+end
+class TestPort < Struct.new(:label, :record, :position)
+  include MFactor::DotNode
+  def dot_record
+    [ record, position ]
   end
-  def parent_nodes
-    parents||[]
-  end
-  def sibling_nodes
-    siblings||[]
+  def dot_label
+    label
   end
 end
-class TestPort<TestNode
-  attr_accessor :record
-  attr_accessor :position
-  def dot_record
-    [ @record, @position ]
+class TestIfNode<TestNode
+  def dot_node_shape
+    return "diamond"
   end
+end
+class TestRecordNode
+  include MFactor::DotRecord
 end
 
 class DotTest < Test::Unit::TestCase
-  def setup
+  def setup_d_graph
     @n1=TestNode.new("n1")
     @n2=TestNode.new("n2")
     @n3=TestNode.new("n3")
     @n4=TestNode.new("n4")
-    @n1.children = [@n2]
-    @n2.children = [@n3]
-    @n3.parents = [@n4]
-    @n4.parents = [@n1]
+    @n1.add_child @n2
+    @n2.add_child @n3
+    @n3.add_parent @n4
+    @n4.add_parent @n1
+  end
+  def test_creation
+    n1=TestNode.new("asd")
+    assert_block do
+      n1.is_a? MFactor::DotNode
+    end
+    assert_respond_to n1, :add_parent
+    assert_respond_to n1, :add_child
+    assert_respond_to n1, :add_sibling
   end
   def test_mixin
+    setup_d_graph
     nodes,transitions,node_names,records = @n1.collect
-    assert_equal nodes,[@n1,@n2,@n4,@n3].to_set
+    assert_equal [@n1,@n2,@n4,@n3].to_set, nodes
   end
   def gen_dot_file(node,fname)
     tf=Tempfile.open(["test","dot"]) do |f|
-      node.dot_code(f)
+      node.dot_graph(f)
       f.close
       df="d:/temp/test_dot_#{fname}.dot"
       cp f.path, df
       `dot -Tpng #{df} -o d:/temp/test_dot_#{fname}.png`
     end
   end
-  def _test_simple_graph
+  def test_simple_graph
+    setup_d_graph
     gen_dot_file(@n1,"sgraph")
   end
-  def _test_records
+  def test_records
     i1=TestPort.new("i1")
     i2=TestPort.new("i2")
     nl=TestPort.new("opname")
     o1=TestPort.new("o1")
     o2=TestPort.new("o2")
     d=TestNode.new("d")
-    o1.children=[d]
-    o1.siblings=[i1,i2,nl,o2]
-    [i1,i2,nl,o1,o2].each_with_index do |p,i|
-      p.position=i
-      p.record=:rec
-    end
+    rec = TestRecordNode.new
+    o1.add_child d
+    o1.add_sibling i1,i2,nl,o2
+    rec.add_port i1,i2,nl,o1,o2
     gen_dot_file(o1,"rectest")
+  end
+  def test_node_shape
+    ni=TestIfNode.new("c==1")
+    ni.add_child TestNode.new("c1"), TestNode.new("c2")
+    gen_dot_file(ni,"shapetest")
   end
 end
