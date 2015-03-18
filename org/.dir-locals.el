4040d67dbd861b120cf8fe76448ade1ecec209fc
m.bruestel <m.bruestel@klippel.de>
2015-03-18 11:59:32 +0100

    documentation update
    - overview
    - usage from project
    - host compiler principle

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -5,6 +5,18 @@
 			  ,(concat (file-name-directory (or load-file-name buffer-file-name))
 				   (file-name-as-directory "..")
 				   (file-name-as-directory "html"))
+                          :publishing-function org-html-publish-to-html
 			  :recursive t
-			  :publishing-function org-publish-org-to-html
-			  )))))))
+			  )
+                         ("org-mfactor-static"
+                          :base-directory ,(file-name-directory (or load-file-name buffer-file-name))
+                          :base-extension "png\\|jpg\\|gif"
+			  :publishing-directory
+			  ,(concat (file-name-directory (or load-file-name buffer-file-name))
+				   (file-name-as-directory "..")
+				   (file-name-as-directory "html"))
+                          :publishing-function org-publish-attachment
+			  :recursive t
+                          )
+                         ("org" :components ("org-mfactor" "org-mfactor-static")))))
+         (eval . (setq org-confirm-babel-evaluate (lambda (lang body) (not (string= lang "ditaa"))))))))
